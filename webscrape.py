732d8f6f8f6996b41a384664d3fa1904563a81a5
Trevor Hegarty <tomheg97@protonmail.com>
2020-05-07 11:50:14 -0500

    2.0 is my code interacting with Wilmott.com. Good little project, very pedogogical.

--- webscrape.py
+++ webscrape.py
@@ -21,21 +21,21 @@ for p_job in python_jobs:
     print(p_job.text.strip())
     print(f"Apply here: {link}\n")
 
-#
-#for job_elem in job_elems:
-#    # Each job_elem is now a new BeautifulSoup object
-#    title_elem = job_elem.find('h2', class_='title')
-#    company_elem = job_elem.find('div', class_='company')
-#    location_elem = job_elem.find('div', class_='location')
-#    # To skip over problamatic elements with NONE 
-#    if None in (title_elem, company_elem, location_elem):
-#        continue
-#    # As a BeautifulSoup object you can perform a text-only action on html elements
-#    print(title_elem.text.strip())
-#    print(company_elem.text.strip())
-#    print(location_elem.text.strip())
-#    print()
-#
+
+for job_elem in job_elems:
+    # Each job_elem is now a new BeautifulSoup object
+    title_elem = job_elem.find('h2', class_='title')
+    company_elem = job_elem.find('div', class_='company')
+    location_elem = job_elem.find('div', class_='location')
+    # To skip over problamatic elements with NONE 
+    if None in (title_elem, company_elem, location_elem):
+        continue
+    # As a BeautifulSoup object you can perform a text-only action on html elements
+    print(title_elem.text.strip())
+    print(company_elem.text.strip())
+    print(location_elem.text.strip())
+    print()
+
 
 #print(results.prettify())
 #pprint(page_content)
